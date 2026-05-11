250eabccb136e50f78c7900e1fe52d7fb019a2b8
Oladeji Raji <69060613+Oladejiraji@users.noreply.github.com>
2026-05-11 23:17:16 +0100

    mount Toaster in local app (#764)

--- apps/local/src/routes/__root.tsx
+++ apps/local/src/routes/__root.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { createRootRoute } from "@tanstack/react-router";
 import { ExecutorProvider } from "@executor-js/react/api/provider";
 import { ExecutorPluginsProvider } from "@executor-js/sdk/client";
+import { Toaster } from "@executor-js/react/components/sonner";
 import { plugins as clientPlugins } from "virtual:executor/plugins-client";
 import { Shell } from "../web/shell";
 
@@ -14,6 +15,7 @@ function RootComponent() {
     <ExecutorProvider>
       <ExecutorPluginsProvider plugins={clientPlugins}>
         <Shell />
+        <Toaster />
       </ExecutorPluginsProvider>
     </ExecutorProvider>
   );
